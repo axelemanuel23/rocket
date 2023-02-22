d222571b15f71b56f2338a1bde624accbf442ce5
Axel Emanuel <100535827+axelemanuel23@users.noreply.github.com>
2023-02-22 20:17:59 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,11 @@ fn index() -> &'static str {
     "Hello, from Rocket!"
 }
 
+#[post("/")]
+fn index() -> &'static str {
+    "Esto es un post"
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
